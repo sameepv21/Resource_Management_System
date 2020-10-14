d684ad1cbe4b2e2556b5f78f1a87456a219808d0
sameep <sameep.v@ahduni.edu.in>
2020-10-14 14:48:01 +0530

    Added Buttons that require css

--- frontend/src/Components/UserPosts.js
+++ frontend/src/Components/UserPosts.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import axios from 'axios';
 import Header from "./Header";
-import { Card, CardHeader, CardBody, CardImg } from 'reactstrap';
+import { Card, CardHeader, CardBody, CardImg, CardFooter, Button } from 'reactstrap';
 
 function ShowPost({particularPostDetail}) {
     //alert(JSON.stringify(particularPostDetail));
@@ -9,11 +9,19 @@ function ShowPost({particularPostDetail}) {
     return(
         <div className="container">
             <Card className="col-10 m-2">
-                <CardHeader><div className="d-flex justify-content-center"><h3>{particularPostDetail.title}</h3></div></CardHeader>
+                <CardHeader className="bg-white"><div className="d-flex justify-content-center"><h3>{particularPostDetail.title}</h3></div></CardHeader>
                 <CardBody>
                     Description: {particularPostDetail.description}<br />
                     Want to view the site? <a href={particularPostDetail.url} >Click Me</a>
                 </CardBody>
+                <div className="d-flex justify-content-center">
+                    <CardFooter className="bg-white">
+                        <Button className="btn m-1" color="warning">Edit<span className="ml-2 fa fa-pencil"></span></Button>
+                        <Button className="btn m-1" color="success" href="./Header.js" download>Download<span className="ml-2 fa fa-download"></span> </Button>
+                        <Button className="btn m-1" color="danger">Delete this Post<span className="ml-2 fa fa-trash-o"></span></Button>
+                        <Button className="btn m-1" color="primary">Add to saved posts<span className="ml-2 fa fa-bookmark"></span></Button>
+                    </CardFooter>
+                </div>
             </Card>
         </div>
     );
@@ -51,14 +59,15 @@ class UserPosts extends Component{
         });
         if(this.state.redirectVar) {
             return(
-                <div>
+                <div className="bg">
                     <Header />
                     {display}
+                    <br /><br /><br /><br /><br /><br /><br />
                 </div>
             );
         } else {
             return(
-                <div>
+                <div className="bg">
                     <Header />
                 </div>
             )
